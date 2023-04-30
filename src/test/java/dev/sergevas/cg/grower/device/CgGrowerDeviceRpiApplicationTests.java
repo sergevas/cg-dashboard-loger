aea840af73b27d838e2d8640f69ea671e1c78473
sergevas <brighterr@gmail.com>
2023-05-01 00:49:13 +0300

    Update Spring Boot version & fix test

--- src/test/java/dev/sergevas/cg/grower/device/CgGrowerDeviceRpiApplicationTests.java
+++ src/test/java/dev/sergevas/cg/grower/device/CgGrowerDeviceRpiApplicationTests.java
@@ -1,13 +1,33 @@
 package dev.sergevas.cg.grower.device;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static dev.sergevas.cg.grower.device.CgGrowerDeviceRpiApplication.buildApp;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CgGrowerDeviceRpiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private ConfigurableApplicationContext applicationContext;
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = buildApp().run();
+        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        applicationContext.close();
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
